23cea80ed01e6e43e413a1ac44b51a86d0658ac0
John Brian <johnbryanjb011@gmail.com>
2019-10-25 15:44:00 +0300

    added js function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function read(){
     var name= document.getElementById("name").value
     console.log(name)
+    var date = parseInt(document.getElementById("date").value);
+    console.log(date);
+
+if (date<=0 && date>=32){
+    alert("invalid date")
+    
+}
+else{
+    console.log("something went wrong")
+}
 }
