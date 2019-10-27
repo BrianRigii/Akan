dfb7269bf9864f881180c7a71b5e1e520c28d9cd
John Brian <johnbryanjb011@gmail.com>
2019-10-27 22:15:46 +0300

    added script to check day

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ function read(){
     console.log(name)
     var date = parseInt(document.getElementById("date").value);
     console.log(date);
+    var month= document.getElementById("month").value
+    console.log(month)
 
 if (date <=0 || date>=32 ){
     alert("invalid date")
     
 }
+var birthday = new Date (month +"-"+date)
+
+
+let dt = birthday.getDay();
+console.log(dt)
+
+
 }
