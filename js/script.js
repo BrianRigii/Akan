6f7fedf5d5472605d068580d2b0597b985ff78eb
John Brian <johnbryanjb011@gmail.com>
2019-10-27 22:23:47 +0300

    added akan names and days arrays

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+var maleAkanName = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+  ];
+  var femaleAkanName = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+  ];
+  var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 function read(){
     var name= document.getElementById("name").value
     console.log(name)
